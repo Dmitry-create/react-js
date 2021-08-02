bd3e1830fe40ccf05a4855c6834275da3d32d0cb
Dmitry <Shirdiman@mail.ru>
2021-08-02 18:02:00 +0300

    create les_1

---
+++ fthproject/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App(props) {
+  return (
+    <div className="App">
+      <header className="App-header" style={{}}>
+        hello my name is {props.name} this is my first step in learning react
+      </header>
+    </div>
+  );
+}
+
+export default App;
